c823f808f9d85e53a116d290a1a3eb3534456db4
advence-liz <1012515183@qq.com>
2019-05-10 15:41:57 +0800

    add: animation demo

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,6 +1,7 @@
 import Taro, { Component, Config } from '@tarojs/taro'
 import { View, Text, Button } from '@tarojs/components'
 import './index.scss'
+import { AnyPtrRecord, AnyPtrRecord } from 'dns'
 
 export default class Index extends Component {
   /**
@@ -14,30 +15,78 @@ export default class Index extends Component {
     navigationBarTitleText: '首页'
   }
   state = {
-    animationData: {}
+    animationScale: [],
+    isScale: false,
+    animationFade: [],
+    isFade: false,
+    animationSlide: Taro.createAnimation({
+      duration: 1000,
+      timingFunction: 'ease'
+      // transformOrigin: '0 0'
+    })
+      .translate3d(0, '100%', 0)
+      .step()
+      .export(),
+    isSlide: false
   }
-  animation: Animation
+  animation: any
 
+  toggleScale = () => {
+    const animation = Taro.createAnimation({
+      duration: 1000,
+      timingFunction: 'ease'
+      // transformOrigin: '0 0'
+    })
+
+    this.state.isScale
+      ? animation.scale(1, 1).step()
+      : animation.scale(0.1, 0.1).step()
+
+    this.setState({
+      isScale: !this.state.isScale,
+      animationScale: animation.export()
+    })
+  }
+  toggleFade = () => {
+    const name = 'Fade'
+    const animation = Taro.createAnimation({
+      duration: 1000,
+      timingFunction: 'ease'
+      // transformOrigin: '0 0'
+    })
+
+    this.state[`is${name}`]
+      ? animation.opacity(1).step()
+      : animation.opacity(0).step()
+
+    this.setState({
+      [`is${name}`]: !this.state[`is${name}`],
+      [`animation${name}`]: animation.export()
+    })
+  }
+  toggleSlide = () => {
+    const name = 'Slide'
+    const animation = Taro.createAnimation({
+      duration: 1000,
+      timingFunction: 'ease'
+    })
+
+    this.state[`is${name}`]
+      ? animation.translate3d(0, '100%', 0).step()
+      : animation.translate3d(0, 0, 0).step()
+
+    this.setState({
+      [`is${name}`]: !this.state[`is${name}`],
+      [`animation${name}`]: animation.export()
+    })
+  }
   componentWillMount() {}
 
   componentDidMount() {}
 
   componentWillUnmount() {}
 
-  componentDidShow() {
-    const animation = Taro.createAnimation({
-      duration: 1000,
-      timingFunction: 'ease'
-    })
-
-    this.animation = animation
-
-    animation.scale(2, 2).step()
-
-    this.setState({
-      animationData: animation.export()
-    })
-  }
+  componentDidShow() {}
 
   componentDidHide() {}
 
@@ -45,10 +94,25 @@ export default class Index extends Component {
     return (
       <View className='index'>
         <View
-          className="box"
-          animation={this.state.animationData}
+          className='box'
+          animation={this.state.animationScale}
+          onClick={this.toggleScale}
         >
-          <Text>Hello world!</Text>
+          <Text>Scale</Text>
+        </View>
+        <View
+          className='box'
+          animation={this.state.animationFade}
+          onClick={this.toggleFade}
+        >
+          <Text>Fade</Text>
+        </View>
+        <View
+          className='box'
+          animation={this.state.animationSlide}
+          onClick={this.toggleSlide}
+        >
+          <Text>Slide</Text>
         </View>
       </View>
     )
